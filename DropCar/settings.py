a0e535192314c2980b374f386798267c81b81b27
Prashant Tamang <np03cs4a210037@heraldcollege.edu.np>
2024-03-26 17:06:56 +0545

    admin inventory modify

--- DropCar/settings.py
+++ DropCar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +141,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info.DropCar@gmail.com'
 EMAIL_HOST_PASSWORD = 'pdldttqyvpcecywb'
+STATIC_URL= "/static/"
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+MEDIA_URL="/media/"
--- inventory/models.py
+++ inventory/models.py
@@ -7,6 +7,7 @@ class Vehicle(models.Model):
     vehicle_type = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    recommended_places = models.CharField(max_length=500, null= True)
     image = models.ImageField(upload_to='vehicle_images/')
 
     def __str__(self):
